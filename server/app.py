2bacd4b4127763f185d8bfeab939a9586f52f9df
Nurdin Ismail <nurdin.ismail@student.moringaschool.com>
2023-09-23 01:07:36 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -10,6 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+
 migrate = Migrate(app, db)
 
 db.init_app(app)
@@ -20,19 +21,56 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakery_dict = bakery.to_dict()
+        bakeries.append(bakery_dict)
+
+    response = make_response(
+        jsonify(bakeries),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+    
+    return response
+
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
+
+    bakery_dict = bakery.to_dict()
+
+    response = make_response(
+        jsonify(bakery_dict),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = []
+    all_baked_goods = BakedGood.query.order_by(BakedGood.price.desc())
+    for baked_good in all_baked_goods:
+        baked_good_dictionary = baked_good.to_dict()
+        baked_goods.append(baked_good_dictionary)
+
+    response = make_response(
+        jsonify(baked_goods), 200, {"Content-Type": "application/json"}
+    )
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+    baked_good_dictionary = baked_good.to_dict()
+    response = make_response(
+        jsonify(baked_good_dictionary), 200, {"Content-Type": "application/json"}
+    )
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
